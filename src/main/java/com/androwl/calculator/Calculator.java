4625fb5b4f0865bfe6ac1311128e89eb27d58843
androol <multiad05@gmail.com>
2024-01-13 20:11:04 +0900

    Add hazelcast

--- src/main/java/com/androwl/calculator/Calculator.java
+++ src/main/java/com/androwl/calculator/Calculator.java
@@ -1,10 +1,20 @@
 package com.androwl.calculator;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class Calculator {
+  @Cacheable("sum")
   public int sum(int a, int b) {
+    try {
+      Thread.sleep(3000);
+    }
+    catch (InterruptedException e) {
+      log.info("Error: ", e);
+    }
     return a + b;
   }
 }
